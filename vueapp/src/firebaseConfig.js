e0beabe7981474244b14adb050247a5a7890e6cb
Paul Janiszewski <pauljaniszewski@Pauls-MacBook-Pro.local>
2020-02-27 21:28:17 -0800

    added firebase project

---
+++ vueapp/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+export default {
+    apiKey: "AIzaSyB1JfnIM62P2ggb89RcPnw6CQF_rtEFcqE",
+    authDomain: "march-madness-pte-claire.firebaseapp.com",
+    databaseURL: "https://march-madness-pte-claire.firebaseio.com",
+    projectId: "march-madness-pte-claire",
+    storageBucket: "march-madness-pte-claire.appspot.com",
+    messagingSenderId: "897741559361",
+    appId: "1:897741559361:web:54b19db9bdafcca85118c0",
+    measurementId: "G-YPK32QXJYP"
+
+}
--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
 
