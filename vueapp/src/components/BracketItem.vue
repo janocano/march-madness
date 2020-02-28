232483689f09087a15f52fa5d38d485d24ddde87
janocano <j_janiszewski@live.com>
2020-02-27 22:08:58 -0500

    initial bracket item component made

---
+++ vueapp/src/components/BracketItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div 
+        class="bracketItem"
+    >
+        {{ team.name }}
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        team: {
+            type: Object,
+            required: true,
+            validator(value) {
+                return value.hasOwnProperty('id') && value.hasOwnProperty('name');
+            }
+        }
+    },
+    
+}
+</script>
+<style scoped>
+.bracketItem{
+    height: 20px;
+    width: 100px;
+    background-color: #fff;
+    font-size: 12px;
+    font-style: italic;
+    color: #000;
+}
+
+</style>
