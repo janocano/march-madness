c7207b33cd882ae46d1d51ec524284c4d5e07449
janocano <j_janiszewski@live.com>
2020-03-03 20:37:40 -0500

    add bracket game node component

--- march-madness-app/src/components/DashboardPage.vue
+++ march-madness-app/src/components/DashboardPage.vue
@@ -2,10 +2,32 @@
     <div id="dashboardPage">
        <!-- navbar -->
        <!-- content -->
+        <bracket-game-node :game="testGame" />
     </div>
 </template>
 <script>
+import BracketGameNode from "./BracketGameNode.vue";
 export default {
+    components: {
+        BracketGameNode
+    },
+    data() {
+        return {
+            testGame: {
+                id: 1,
+                first_team: {
+                    id: 1,
+                    name: "ASU",
+                },
+                second_team: {
+                    id: 2,
+                    name: "Duke",
+                },
+                winner_id: 1
+            }
+        };
+    },
+    
     
 }
 </script>
